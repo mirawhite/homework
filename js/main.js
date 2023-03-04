2d7e18ae6d52ef7f5903716fa1f8977672da66d6
mirawhite <125466784+mirawhite@users.noreply.github.com>
2023-03-04 18:26:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    var mixer = mixitup('.directions__list');
+
+    $('.directions__filter-btn').on('click', function () {
+        $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+        $(this).addClass('directions__filter-btn--active')
+    })
+
+    $('.team__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        infinite: true,
+        draggable: false,
+        waitForAnimate: false,
+    })
+    $('.team__slider-prev').on('click', function (e) {
+        e.preventDefault()
+        $('.team__slider').slick('slickPrev')
+    })
+    $('.team__slider-next').on('click', function (e) {
+        e.preventDefault()
+        $('.team__slider').slick('slickNext')
+    })
+    
+    $('.testimonials__slider').slick({
+        arrows: false,
+        dots: true, 
+        appendDots: $('.testimonials__dots'),
+        waitForAnimate: false,
+    })
+
+    $('.testimonials__prev').on('click', function (e) {
+        e.preventDefault()
+        $('.testimonials__slider').slick('slickPrev')
+    })
+    $('.testimonials__next').on('click', function (e) {
+        e.preventDefault()
+        $('.testimonials__slider').slick('slickNext')
+    })
+
+
+})
